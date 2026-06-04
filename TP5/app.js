cd8ef06949224f805ff0dddd160841bed1654768
alumno <51085862@ivt.edu.ar>
2026-06-04 09:07:54 -0300

    04/06

---
+++ TP5/app.js
@@ -0,0 +1,29 @@
+//Ejercicio 1a
+let num1=10 
+let num2=20
+let suma = num1 + num2
+console.log (suma)
+
+//Ejercicio 1b
+let n3=40
+let n4=20
+let resta= n3 - n4 
+console.log (resta) 
+
+//Ejercicio 1c
+let n5=2
+let n6=5
+let mult= n5 * n6
+console.log (mult)
+
+//Ejercicio 1d
+let n7=10
+let n8= 2
+let div= n7 / n8
+console.log (div) 
+
+// Ejercicio 2 
+alert (suma)
+alert (resta)
+
+//Ejercicio 3
