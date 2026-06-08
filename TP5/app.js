d7915c2ad2b4f5ceafb7cbe010f4a8007ae17189
alumno <51085862@ivt.edu.ar>
2026-06-08 10:30:02 -0300

    Update app.js

--- TP5/app.js
+++ TP5/app.js
@@ -2,13 +2,13 @@
 let num1=10 
 let num2=20
 let suma = num1 + num2
-console.log (suma)
+console.log ('la suma de ' +' '+ num1 + 'y'+ num2 +'es'+ suma)
 
 //Ejercicio 1b
 let n3=40
 let n4=20
 let resta= n3 - n4 
-console.log (resta) 
+console.log ( resta) 
 
 //Ejercicio 1c
 let n5=2
@@ -26,4 +26,5 @@ console.log (div)
 alert (suma)
 alert (resta)
 
-//Ejercicio 3
+//Ejercicio 3a 
+
