b1bd09f5add92639df769cc8a205279cad422079
cataaagr237 <51085862@ivt.edu.ar>
2026-06-08 11:14:11 -0300

    Update app.js

--- TP5/app.js
+++ TP5/app.js
@@ -2,29 +2,34 @@
 let num1=10 
 let num2=20
 let suma = num1 + num2
-console.log ('la suma de ' +' '+ num1 + 'y'+ num2 +'es'+ suma)
+console.log ("el resultado de la suma es " + suma)
 
 //Ejercicio 1b
 let n3=40
 let n4=20
 let resta= n3 - n4 
-console.log ( resta) 
+console.log ("el resultado de la resta es " + resta) 
 
 //Ejercicio 1c
 let n5=2
 let n6=5
 let mult= n5 * n6
-console.log (mult)
+console.log ("el resultado de la mult es " +  mult)
 
 //Ejercicio 1d
 let n7=10
 let n8= 2
 let div= n7 / n8
-console.log (div) 
+console.log ( "el resultado de la div es " + div) 
 
 // Ejercicio 2 
-alert (suma)
-alert (resta)
+alert ("el resultado de la suma es " + suma)
+alert ( "el resultado de la resta es " + resta)
 
-//Ejercicio 3a 
+//Ejercicio 4a
 
+//Ejercicio 4b 
+
+// ejercicio 4c
+
+//ejercicio 4d
