b43bd9edf71773cbd1faff8cd837a76c64f46f0b
alumno <51085862@ivt.edu.ar>
2026-06-11 08:07:31 -0300

    Update app.js

--- TP5/app.js
+++ TP5/app.js
@@ -27,9 +27,33 @@ alert ("el resultado de la suma es " + suma)
 alert ( "el resultado de la resta es " + resta)
 
 //Ejercicio 4a
-
+let nombreAlumno="Lucia"
 //Ejercicio 4b 
-
+let apellidoAlumno="Hernandez"
 // ejercicio 4c
-
+let edadAlumno="16"
 //ejercicio 4d
+ console.log("el alumno" + nombreAlumno + apellidoAlumno + "tiene" + edadAlumno);
+
+ //Ejercicio 5a
+ let precio1=20
+ let precio2=10
+ let precio3=5
+ //Ejercicio 5b
+ let total= precio1 + precio2 + precio3
+ //Ejercicio 5c
+  console.log("el total a pagar es" + total);
+//Ejercicio 6a
+let comidaFavorita="Sushi"
+let colorfavorito="Celeste"
+let numeroFavorito= "25"
+//Ejercicio 6b
+console.log("mi comida favorita es" + comidaFavorita + "mi color favorito es" + colorfavorito + "mi numero favorito es" + numeroFavorito);
+//Ejercicio 7a
+let pais= "Argentina"
+ //Ejercicio 7b
+ console.log(pais);
+ //Ejercicio 7c
+  pais= "Brasil"
+ //Ejercicio 7d
+ console.log(pais);
