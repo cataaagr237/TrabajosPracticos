976caa1adc69aa1b76143a2dc0ec49272d3b1f3d
alumno <51085862@ivt.edu.ar>
2026-07-02 08:44:22 -0300

    entrega

--- tp6/app.js
+++ tp6/app.js
@@ -1,33 +1,38 @@
-let boton1=document.querySelector ( "#boton1")
-let boton2= document.querySelector ("#boton2")
-let boton3= document.querySelector ("#boton3")
-let boton4= document.querySelector ("#boton4")
+let boton1 = document.querySelector("#boton1")
+let boton2 = document.querySelector("#boton2")
+let boton3 = document.querySelector("#boton3")
+let boton4 = document.querySelector("#boton4")
 
-let p= document.querySelector ("#parrafo")
-boton1.onclick= function() {
-p.textContent= " hola soy cata"
-
+let p = document.querySelector("#parrafo")
+boton1.onclick = function () {
+    p.textContent = " hola soy cata"
 }
 
-boton2.onclick= function(){
-document.body.style.backgroundColor= "pink"
-
+boton2.onclick = function () {
+    p.style.backgroundColor = "pink"
 }
 
-boton3.onclick= function() {
-p.style.color= "red"
-}
-
-
-boton4.onclick= function(){
-
-p.style.fontFamily= "Courier New"
-
-
+boton3.onclick = function () {
+    p.style.color = "red"
 }
 
 
+boton4.onclick = function () {
 
+    p.style.fontFamily = "Courier New"
+}
+
+boton5.onclick= function (){
+p.style.fontSize= "30px"
+}
+
+boton6.onclick = function(){
+    parrafo.textContent= " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aperiam iure facilis, tempore asperiores ea officiis odio quis magni, suscipit assumenda, exercitationem reprehenderit minima similique accusamus cupiditate ipsa? Voluptates, at."
+    parrafo. style. fontSize= "100%"
+    parrafo.style.color= "black"
+    parrafo.style.backgroundColor= "purple"
+parrafo.style.fontFamily= "Arial"
+}
 
 
 
