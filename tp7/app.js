9ce2e5a0a361b859474b0358c7bc2482091e12d9
alumno <51085862@ivt.edu.ar>
2026-07-30 09:11:48 -0300

    30.07

---
+++ tp7/app.js
@@ -0,0 +1,20 @@
+let  ejercicio1 = document.querySelector ("#ejercicio1")
+let boton1 = document.querySelector ("#boton1ejercicio1")
+let edad= 18
+
+boton1ejercicio1.onclick = function () 
+    if (edad >= 18) {
+ejercicio1.textContent= "sos mayor de edad"
+ } else {
+     ejercicio1.textContent = "sos menor de edad "
+ }
+
+
+
+
+
+
+
+
+
+
